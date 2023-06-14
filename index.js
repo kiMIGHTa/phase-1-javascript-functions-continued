3d1516932c2b8203e6cdaa5cacf31f77e95a6db7
Dennis Kimaita <dkimaita22@gmail.com>
2023-06-14 19:40:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity=`roller-skate`){
+    return `This Saturday, I want to ${activity}!`
+}
+
+let mondayWork = function(mondayActivity=`go to the office`){
+    return `This Monday, I will ${mondayActivity}.`
+}
+
+function wrapAdjective(flair=`*`){
+    return function encouragingPrompt(adjective=`special`){
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
